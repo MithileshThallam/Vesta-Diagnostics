d111ca76bd16f643b79f32c669f1fd7d3fad9c69
unknown <rnamit099@gmail.com>
2025-07-27 09:50:26 +0530

    removed header from login and sign up pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,10 @@ import { Toaster } from "./components/ui/toaster"
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from '@/pages/Home'
 import Login from "@/pages/Login";
-import  Signup from "@/pages/SignUp";
+import Signup from "@/pages/SignUp";
 import Test from "./pages/Test";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
@@ -14,23 +14,34 @@ import NotFound from "@/pages/NotFound"
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-      <Header />
-        <Routes>
+const AppContent = () => {
+  const location = useLocation();
+  const hideHeaderPaths = ['/login', '/signup'];
+  const shouldShowHeader = !hideHeaderPaths.includes(location.pathname);
+
+  return (
+    <>
+      {shouldShowHeader && <Header />}
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/tests" element={<Test />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contactus" element={<Contact />} />
-
         <Route path="*" element={<NotFound />} />
-        </Routes>
+      </Routes>
+    </>
+  );
+};
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <AppContent />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
